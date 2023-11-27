6701b955128fd3e8a18c067b643d032797f7d259
Georgios <geor.oikonomopoulos@gmail.com>
2023-11-27 23:54:41 +0200

    get by name

--- src/app/http/countries.service.ts
+++ src/app/http/countries.service.ts
@@ -7,10 +7,15 @@ import { Country } from './country';
   providedIn: 'root'
 })
 export class CountriesService {
-  readonly url = 'https://restcountries.com/v3.1/all'
+  readonly url = 'https://restcountries.com/v3.1/all';
+  readonly urlName = 'https://restcountries.com/v3.1/name/';
   constructor(private http: HttpClient) { }
 
   getCountries(): Observable<Country[]> {
     return this.http.get<Country[]>(this.url);
+  }
+
+  getByName(name: string): Observable<Country> {
+    return this.http.get<Country>(this.urlName + name);
   }
 }
