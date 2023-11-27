825132c36447dee86c135405436a16ff9b2783d9
Georgios <geor.oikonomopoulos@gmail.com>
2023-11-27 22:59:01 +0200

    solution

--- src/app/services/mapper.service.spec.ts
+++ src/app/services/mapper.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MapperService } from './mapper.service';
+import { PeopleService } from '../people.service';
+import { PrefixService } from './prefix.service';
 
 describe('MapperService', () => {
   let service: MapperService;
+  let people: PeopleService;
+  let prefix: PrefixService;
+  const prefixSpy = {
+    getPrefix: jest.fn()
+  }
+
+  const peopleSpy = {
+    getPeople: jest.fn()
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: PeopleService, useValue: peopleSpy },
+      { provide: PrefixService, useValue: prefixSpy }]
+    });
     service = TestBed.inject(MapperService);
+    people = TestBed.inject(PeopleService);
+    prefix = TestBed.inject(PrefixService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  
+  it('should return array of ng_John', () => {
+    peopleSpy.getPeople.mockReturnValue(['John']);
+    prefixSpy.getPrefix.mockReturnValue(['ng_']);
+    expect(service.mapValues()).toEqual(['ng_John']);
+  });
+  
+  it('should return array of Maria', () => {
+    peopleSpy.getPeople.mockReturnValue(['Maria']);
+    prefixSpy.getPrefix.mockReturnValue(['']);
+    expect(service.mapValues()).toEqual(['Maria']);
   });
 });
