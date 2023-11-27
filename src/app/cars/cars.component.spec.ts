4f569c56c8b2f94d518aa3ccb09055e64db489a8
Georgios <geor.oikonomopoulos@gmail.com>
2023-11-28 01:11:41 +0200

    car solution

--- src/app/cars/cars.component.spec.ts
+++ src/app/cars/cars.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarsComponent } from './cars.component';
+import { CarService } from '../car.service';
+
+class Mock {
+  getCars() {
+    return [
+      {
+        brand: 'VW',
+        year: 1994
+      }, {
+        brand: 'Opel',
+        year: 2000
+      },
+    ]
+  }
+}
 
 describe('CarsComponent', () => {
   let component: CarsComponent;
@@ -8,14 +23,17 @@ describe('CarsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CarsComponent]
+      imports: [CarsComponent],
+      providers: [{ provide: CarService, useClass: Mock }]
     });
     fixture = TestBed.createComponent(CarsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('The year of the second car is 2000', () => {
+    const li = fixture.debugElement.nativeElement.querySelectorAll('li')[1];
+    const year = li.querySelectorAll('span')[1];
+    expect(year.textContent).toEqual('2000');
   });
 });
