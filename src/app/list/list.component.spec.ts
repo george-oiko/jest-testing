920560f6971e0495fea7b43cc53e45fc8f9ec8bf
Georgios <geor.oikonomopoulos@gmail.com>
2023-12-05 08:56:25 +0200

    solution

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { By } from '@angular/platform-browser';
+import { first } from 'rxjs';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -17,5 +19,40 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('titile is list', () => {
+    component.title = 'list';
+    fixture.detectChanges();
+
+    let title = fixture.debugElement.query(By.css('h1'))
+    expect(title.nativeElement.textContent).toEqual('list');
+    expect(component.title).toEqual('list');
+
+  });
+  
+  it('list Input works as expected', () => {
+    component.list = [{
+      code: 1,
+      description: ''
+    }];
+    fixture.detectChanges();
+
+    expect(component.list.length).toEqual(1);
+    expect(component.list[0].code).toEqual(1);
+  });
+
+  it('refresh button should raise the refreshed event', () => {
+    component.list = [{
+      code: 1,
+      description: ''
+    }];
+    fixture.detectChanges();
+
+    let emission: any;
+    component.refreshed.pipe(first()).subscribe((emit) => (emission = emit));
+    let debugElement = fixture.debugElement.query(By.css('button'))
+    debugElement.triggerEventHandler('click');
+    expect(emission).toBe(undefined);
   });
 });
